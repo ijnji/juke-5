f4dd7c0f1a1f8f513c3129555fa4fa23b1812471
Ran Duan <rnduan@gmail.com>
2016-08-29 15:28:41 -0400

    album-list directive

---
+++ browser/js/album/album.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+juke.directive('albumList', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            albums: '='
+        },
+        templateUrl: '/js/album/templates/album-list.html'
+    }
+});
