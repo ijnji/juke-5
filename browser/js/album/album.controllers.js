778a845e967f9c4f1b635ed601dc4630daa196d9
devpatel91 <devnitinpatel91@gmail.com>
2016-08-29 16:31:37 -0400

    Upto song list

--- browser/js/album/album.controllers.js
+++ browser/js/album/album.controllers.js
@@ -5,25 +5,25 @@
 
 juke.controller('AlbumCtrl', function ($scope, PlayerFactory, theAlbum) {
 
-  $scope.album = theAlbum;
+   $scope.album = theAlbum;
 
-  $scope.toggle = function (song) {
-    if (song !== PlayerFactory.getCurrentSong()) {
-      PlayerFactory.start(song, $scope.album.songs);
-    } else if ( PlayerFactory.isPlaying() ) {
-      PlayerFactory.pause();
-    } else {
-      PlayerFactory.resume();
-    }
-  };
+  // $scope.toggle = function (song) {
+  //   if (song !== PlayerFactory.getCurrentSong()) {
+  //     PlayerFactory.start(song, $scope.album.songs);
+  //   } else if ( PlayerFactory.isPlaying() ) {
+  //     PlayerFactory.pause();
+  //   } else {
+  //     PlayerFactory.resume();
+  //   }
+  // };
 
-  $scope.getCurrentSong = function () {
-    return PlayerFactory.getCurrentSong();
-  };
+  // $scope.getCurrentSong = function () {
+  //   return PlayerFactory.getCurrentSong();
+  // };
 
-  $scope.isPlaying = function (song) {
-    return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
-  };
+  // $scope.isPlaying = function (song) {
+  //   return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
+  // };
 
 });
 
---
+++ browser/js/song/song.directive.js
@@ -0,0 +1,35 @@
+'use strict';
+
+juke.directive('songList', function(PlayerFactory) {
+    return {
+        restrict: 'E',
+        scope: {
+            data: '='
+        },
+        templateUrl: '/js/song/templates/song-list.html',
+        link: function(scope) {
+
+            scope.album = scope.data;
+
+            scope.toggle = function(song) {
+                if (song !== PlayerFactory.getCurrentSong()) {
+                    PlayerFactory.start(song, scope.album.songs);
+                } else if (PlayerFactory.isPlaying()) {
+                    PlayerFactory.pause();
+                } else {
+                    PlayerFactory.resume();
+                }
+            };
+
+            scope.getCurrentSong = function() {
+                return PlayerFactory.getCurrentSong();
+            };
+
+            scope.isPlaying = function(song) {
+                return PlayerFactory.isPlaying() && PlayerFactory.getCurrentSong() === song;
+            };
+        }
+       
+    };
+
+});
